6259523314923966039fdb5cd8de3f6174f05109
trubmd <trubmd@gmail.com>
2024-08-02 17:08:25 +0300

    Update melisa_install.sh

--- melisa_install.sh
+++ melisa_install.sh
@@ -3,6 +3,7 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
 source ~/.bashrc
 yes | conda create -n melisa sage python=3.10
+eval "$(conda shell.bash hook)"
 conda activate melisa
 pip install jupyter qtconsole
 pip install PyQt5

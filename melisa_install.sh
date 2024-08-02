2ec8ab4698993799c21f0579ee4631fc75f8cc32
trubmd <trubmd@gmail.com>
2024-08-02 16:52:23 +0300

    Update melisa_install.sh

--- melisa_install.sh
+++ melisa_install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
 source ~/.bashrc
 yes | conda create -n melisa sage python=3.10

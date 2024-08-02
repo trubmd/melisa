32c714e72571e8d5a148f66a7387430e21549069
trubmd <trubmd@gmail.com>
2024-08-02 16:45:14 +0300

    Update melisa_install.sh

--- melisa_install.sh
+++ melisa_install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-sudo apt update
-sudo apt install git
-git clone https://github.com/trubmd/melisa.git
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh
 bash ./Miniconda3-latest-Linux-x86_64.sh -b -p $HOME/miniconda3
@@ -10,5 +7,4 @@ yes | conda create -n melisa sage python=3.10
 conda activate melisa
 pip install jupyter qtconsole
 pip install PyQt5
-cd ..
 conda deactivate
